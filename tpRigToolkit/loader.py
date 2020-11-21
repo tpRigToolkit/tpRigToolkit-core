4763a8fff2a824ba1f356648d8ea2e4df816599a
Tomas Poveda <tpovedatd@gmail.com>
2020-11-21 12:16:42 +0100

    Updated tpRigToolkit loader module

--- tpRigToolkit/loader.py
+++ tpRigToolkit/loader.py
@@ -15,8 +15,14 @@ __email__ = "tpovedatd@gmail.com"
 import os
 import logging.config
 
-import tpDcc
-from tpRigToolkit import register
+import tpDcc.loader as dcc_loader
+from tpDcc.libs.python import contexts
+from tpDcc.core import dcc as core_dcc
+from tpDcc.managers import configs, resources, libs, tools, menus
+from tpDcc.libs.qt.managers import toolsets as qt_toolsets
+
+import tpRigToolkit.config
+import tpRigToolkit.toolsets
 
 # =================================================================================
 
@@ -32,91 +38,44 @@ def init(import_libs=True, dev=False):
     :param dev: bool, Whether tpRigToolkit is initialized in dev mode or not
     """
 
-    if dev:
-        register.cleanup()
-
     logger = create_logger(dev=dev)
 
-    register.register_class('logger', logger)
-
     if import_libs:
-        import tpDcc.loader as dcc_loader
         dcc_loader.init(dev=dev)
 
-    # We do the importer and registering of the classes after ensuring tpDcc is loaded
-    from tpRigToolkit.managers import data as data_manager, names as names_manager, packages as packages_manager
-    from tpRigToolkit.managers import plugins as plugins_manager, scripts as scripts_manager
-    from tpRigToolkit.widgets import window, dialog
-
-    register.register_class('DataMgr', data_manager.DataManagerSingleton)
-    register.register_class('NamesMgr', names_manager.RigToolkitNamesManagerSingleton)
-    register.register_class('PackagesMgr', packages_manager.PackagesManagerSingleton)
-    register.register_class('PluginsMgr', plugins_manager.PluginsManagerSingleton)
-    register.register_class('ScriptsMgr', scripts_manager.ScriptsManagerSingleton)
-    register.register_class('Window', window.MainWindow)
-    register.register_class('Dialog', dialog.MainDialog)
-
-    # skip_modules = ['{}.{}'.format(PACKAGE, name) for name in ['libs', 'tools']]
-    # importer.init_importer(package=PACKAGE, skip_modules=skip_modules)
-
-    init_managers(dev=dev)
-
-    dcc_loader_module = tpDcc.get_dcc_loader_module('tpRigToolkit.dccs')
+    dcc_loader_module = core_dcc.get_dcc_loader_module('tpRigToolkit.dccs')
     if dcc_loader_module:
         dcc_loader_module.init(dev=dev)
 
-
-def init_managers(dev=True):
-    """
-    Initializes all tpDcc managers
-    """
-
-    import tpDcc
-    import tpRigToolkit
-    from tpRigToolkit import config, toolsets
-
-    # Register resources
-    resources_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
-    tpDcc.ResourcesMgr().register_resource(resources_path)
+    register_resources()
 
     # Register configuration files
-    tpDcc.ConfigsMgr().register_package_configs('tpRigToolkit', os.path.dirname(config.__file__))
-
-    core_config = tpDcc.ConfigsMgr().get_config(
-        'tpRigToolkit-core',
-        environment='development' if dev else 'production'
-    )
+    configs.register_package_configs(PACKAGE, os.path.dirname(tpRigToolkit.config.__file__))
+    core_config = configs.get_config('tpRigToolkit-core', environment='development' if dev else 'production')
     if not core_config:
-        tpDcc.logger.warning(
+        logger.warning(
             'tpRigToolkit-core configuration file not found! '
             'Make sure that you have tpRigToolkit-config package installed!')
         return None
 
-    # Register resources
-    resources_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'resources'))
-    tpDcc.ResourcesMgr().register_resource(resources_path)
-
-    # Register libraries
     libs_to_load = core_config.get('libs', list())
-    tpDcc.LibsMgr().register_package_libs(pkg_name=PACKAGE, libs_to_register=libs_to_load, dev=dev)
-    tpDcc.LibsMgr().load_registered_libs(PACKAGE)
-
-    # Register tools
     tools_to_load = core_config.get('tools', list())
-    tpDcc.ToolsMgr().register_package_tools(pkg_name=PACKAGE, tools_to_register=tools_to_load, dev=dev)
-    tpDcc.ToolsMgr().load_registered_tools(PACKAGE)
 
-    # Register toolsets
-    tpDcc.ToolsetsMgr().register_path(PACKAGE, os.path.dirname(os.path.abspath(toolsets.__file__)))
-    tpDcc.ToolsetsMgr().load_registered_toolsets(package_name=PACKAGE, tools_to_load=tools_to_load)
+    with contexts.Timer('Libraries loaded', logger=logger):
+        libs.LibsManager().register_package_libs(PACKAGE, libs_to_register=libs_to_load, dev=dev)
+        libs.LibsManager().load_registered_libs(PACKAGE)
 
-    # Create tpRigToolkit menu
-    # The creation of the menu depends on the registered toolsets
-    tpDcc.MenusMgr().create_menus(package_name=PACKAGE, dev=dev)
+    with contexts.Timer('Tools loaded', logger=logger):
+        tools.ToolsManager().register_package_tools(PACKAGE, tools_to_register=tools_to_load, dev=dev)
+        tools.ToolsManager().load_registered_tools(PACKAGE)
 
-    # Initialize data and scripts manager
-    tpRigToolkit.DataMgr()
-    tpRigToolkit.ScriptsMgr()
+    with contexts.Timer('Toolsets loaded', logger=logger):
+        qt_toolsets.ToolsetsManager().register_path(
+            PACKAGE, os.path.dirname(os.path.abspath(tpRigToolkit.toolsets.__file__)))
+        qt_toolsets.ToolsetsManager().load_registered_toolsets(package_name=PACKAGE, tools_to_load=tools_to_load)
+
+    with contexts.Timer('Menu created', logger=logger):
+        menus.create_menus(package_name=PACKAGE, dev=dev)
 
 
 def create_logger(dev=False):
@@ -138,3 +97,15 @@ def create_logger(dev=False):
             handler.setLevel(logging.DEBUG)
 
     return logger
+
+
+def register_resources():
+    """
+    Registers tpDcc.libs.qt resources path
+    """
+
+    resources_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
+    resources.register_resource(resources_path, key='tpRigToolkit-core')
+
+
+create_logger()
